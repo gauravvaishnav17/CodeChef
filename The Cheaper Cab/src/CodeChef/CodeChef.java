fb9760ed4703cf5e539d142b1e6d96219d038ef7
ANTILA\gauravv <vgaurav@cdac.in>
2022-10-17 16:00:52 +0530

    added solution for The Cheaper Cab problem of codechef

---
+++ The Cheaper Cab/src/CodeChef/CodeChef.java
@@ -0,0 +1,33 @@
+package CodeChef;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc = new Scanner(System.in);
+		int useCase = sc.nextInt();
+	
+	while(useCase!=0){
+	    	int x=sc.nextInt();
+		    int y=sc.nextInt();
+	
+		if(x<y) {
+			System.out.println("FIRST");
+			
+		}
+		else if (y<x) {
+			System.out.println("SECOND");
+		}
+		else {
+			System.out.println("ANY");
+		}
+		useCase--;
+		
+	}
+	}
+}
